c76ecede7827545079e93f443c0a16dd904c75cf
mohamed.elzanaty3 <mohamed.elzanaty3@vodafone.com>
2023-10-28 14:00:48 +0200

    feat: css styling 💄

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import styles from '@/app/ui/home.module.css'
 
 export default function Page() {
   return (
@@ -6,6 +7,8 @@ export default function Page() {
       <div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52'>{/* <AcmeLogo /> */}</div>
       <div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
         <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20'>
+          <div className='h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent' />
+          <div className={styles.shape} />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href='https://nextjs.org/learn/' className='text-blue-500'>
